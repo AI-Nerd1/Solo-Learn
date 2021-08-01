359ab6eb53a2ae88eaba359f594303da29711498
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-08-01 23:09:34 +0300

    python

---
+++ practice.py
@@ -0,0 +1,51 @@
+print('Hello World')
+print("Printing with double quotation marks")
+print('Checking if they the same, this has one quotation mark.')
+
+print(2* (3+4))
+print(10/2)
+print((4+8)/2)
+print(1+2+3+4.0+5)
+print(2**5)
+print(9**(0.5))
+print(8**(1/3))
+
+## Floor division
+
+print('Floor division for 20 and 6 = ',20//6)
+print('20 mod 6 = ', 20%6)
+print(7%(5//2))
+print(0.01*(2**30))
+
+##  String
+
+print("Python is fun!")
+print('Always look on the bright sode of life')
+print('Today is Logan\'s day')
+print('Printing\nNew Line at this point\n How do you feel?')
+print("""This 
+is a 
+multiline 
+text""")
+
+##  Concatenation
+
+print("spam"+"eggs")
+print("spam"+'eggs')
+print('spam'*3)
+print(4 * '2')
+
+##  Variables
+x = 3
+print('sx = ', x)
+print('x + 3 = ', x + 3)
+# name = input('What is your name: ')
+# print('Did you say your name is', name + '?')
+# print('Alright, nice to meet you',name)
+# age = int(input('How old are you? '))
+# print(name, "we need to confirm the fact that you are ", age, ' years old')
+# #print(num:=int(input()))
+ 
+f1 = int(input('First integer: '))
+f2 = int(input('Second integer: '))
+print(f1 + f2)
